2d125a3796eb1c33114eeed7f693a3fe7a8abf9c
Menhythien <dinhthienmenh1505@gmail.com>
2024-10-10 23:04:17 +0700

    Add user routes

---
+++ server/src/routes/UserRoutes.ts
@@ -0,0 +1,27 @@
+import express from 'express';
+import {
+    createUser,
+    getUserById,
+    updateUserById,
+    deleteUserById,
+    listUsers
+} from '../controllers/UserController';
+
+const router = express.Router();
+
+// Route to create a new user
+router.post('/', createUser);
+
+// Route to get a user by ID
+router.get('/:id', getUserById);
+
+// Route to update a user by ID
+router.put('/:id', updateUserById);
+
+// Route to delete a user by ID
+router.delete('/:id', deleteUserById);
+
+// Route to list all users
+router.get('/', listUsers);
+
+export default router;
